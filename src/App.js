4e7c870278b5a14d8bb6805fc9bcc9c7ec8af1f3
Adibah Saffa <adibasaffa98@gmail.com>
2022-12-17 00:31:53 +0800

    init auth

--- src/App.js
+++ src/App.js
@@ -2,14 +2,17 @@ import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 import Home from './pages/Home';
 import Navbar from './components/Navbar';
+import { AuthContextProvider } from './context/AuthContext';
 
 const App = () => {
   return (
     <>
-      <Navbar />
-      <Routes>
-        <Route path='/' element={<Home />} />
-      </Routes>
+      <AuthContextProvider>
+        <Navbar />
+        <Routes>
+          <Route path='/' element={<Home />} />
+        </Routes>
+      </AuthContextProvider>
     </>
   );
 };
